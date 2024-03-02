6f6233f8a6543f0105a3bb24b99d9be051afd1e5
Ariff-dev <ariff.dev24@gmail.com>
2024-03-01 20:06:44 -0600

    feat: Credentials component completed

--- src/components/page/credentials/Credentials.jsx
+++ src/components/page/credentials/Credentials.jsx
@@ -1,5 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 export const Credentials = () => {
-  return <div>Credentials</div>
+  return (
+    <div className='bg-gradient-to-r from-color-bg-secundary to-color-bg p-8 rounded-2xl flex flex-col gap-4'>
+      <div className='flex items-center justify-center'>
+        <Image
+          src='https://res.cloudinary.com/dvppsdp4j/image/upload/v1709344773/firma_ypqduo.png'
+          alt='firma'
+          width={120}
+          height={120}
+        />
+      </div>
+      <div>
+        <p className='font-medium uppercase text-sm text-color-text-sub'>
+          Más sobre mí
+        </p>
+        <h2 className='font-semibold text-2xl'>Credentials</h2>
+      </div>
+    </div>
+  )
 }
