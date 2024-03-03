2aa2b6e16296e77fcf439b2acee5a2dcc019a06a
Ariff-dev <ariff.dev24@gmail.com>
2024-03-02 19:43:40 -0600

    refactor: Styles in Text Component

--- src/components/page/text/Text.tsx
+++ src/components/page/text/Text.tsx
@@ -4,20 +4,19 @@ import './css/text.css'
 export const Text = () => {
   return (
     <div className='bg-gradient-to-tr from-color-bg-secundary to-color-bg p-4 rounded-2xl overflow-hidden'>
-      <p className='text-xs uppercase container-dev moving-text font-medium '>
+      <p className='text-xs uppercase container-dev moving-text font-medium h-full w-full'>
         <span>
-          *Últimos <span className='text-color-text-sub'>trabajos</span> y
-          características*
+          * Últimos <span className='text-color-text-sub'>trabajos</span> y
+          características *
         </span>
         <span>
-          *Últimos <span className='text-color-text-sub'>trabajos</span> y
-          características*
+          * Últimos <span className='text-color-text-sub'>trabajos</span> y
+          características *
         </span>{' '}
         {/* Texto duplicado para el bucle */}
         <span>
-          Rename folder and file *Últimos{' '}
-          <span className='text-color-text-sub'>trabajos</span> y
-          características*
+          * Últimos <span className='text-color-text-sub'>trabajos</span> y
+          características *
         </span>{' '}
         {/* Texto duplicado para el bucle */}
       </p>
